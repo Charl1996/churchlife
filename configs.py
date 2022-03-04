f696caefeeabc6efec187727695f9442f76df973
Charl Smit <csmit@dimagi.com>
2022-03-04 12:31:31 +0200

    Fix datatypes

--- configs.py
+++ configs.py
@@ -9,20 +9,20 @@ configs.read('secrets.ini')
 APPLICATION_ROOT_PATH = Path().resolve(__file__)
 
 """ ======== FastAPI Web ======== """
-APPLICATION_HOST = configs['application']['host']
-APPLICATION_PORT = configs['application']['post']
+APPLICATION_HOST = configs['application']['Host']
+APPLICATION_PORT = int(configs['application']['Port'])
 
 
 """ ======== DATABASE MANAGER ======== """
 DEFAULT_DATABASE_HANDLER = 'breeze'
-BREEZE_BASE_URL = configs['breeze']['base_url']
-BREEZE_API_KEY = configs['breeze']['api_key']
+BREEZE_BASE_URL = configs['breeze']['BaseUrl']
+BREEZE_API_KEY = configs['breeze']['ApiKey']
 
 
 """ ======== MESSAGING ======== """
 DEFAULT_MESSAGING_HANDLER = 'respondio'
-RESPONDIO_BASE_URL = configs['respond.io']['base_url']
-RESPONDIO_API_KEY = configs['respond.io']['api_key']
+RESPONDIO_BASE_URL = configs['respond.io']['BaseUrl']
+RESPONDIO_API_KEY = configs['respond.io']['ApiKey']
 
 
 """ ======== LOGGING ======== """
@@ -32,4 +32,4 @@ CELERY_LOG_FILE = f'{LOGGING_PATH}/celery.log'
 
 
 """ ======== SERVICES ======== """
-RABBITMQ_URL = configs['rabbitmq']['url']
+RABBITMQ_URL = configs['rabbitmq']['Url']
